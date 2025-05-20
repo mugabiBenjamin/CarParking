5f39b2807c5c754d703e5a3edf764cc72719a610
mugabiBenjamin <mugabiben6@gmail.com>
2025-05-20 13:56:37 +0300

    Remove car parking system old files

--- util/Logger.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class Logger {
-    public static void log(String msg) {
-        System.out.println("[LOG] " + msg);
-    }
-}
--- util/Validator.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class Validator {
-    public static boolean isValidPlate(String plate) {
-        return plate != null && plate.matches("[A-Z]{3} \\d{3}[A-Z]");
-    }
-}
