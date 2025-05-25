aaf073ace31040e85f51835370bffcc6bd61ea1e
mugabiBenjamin <mugabiben6@gmail.com>
2025-05-25 16:34:09 +0300

    Add RoundedBorder class for customizable rounded borders in UI components

---
+++ src/view/RoundedBorder.java
@@ -0,0 +1,35 @@
+package view;
+
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class RoundedBorder implements Border {
+    private int radius;
+    private int thickness;
+
+    public RoundedBorder(int radius, int thickness) {
+        this.radius = radius;
+        this.thickness = thickness;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(c.getBackground().darker());
+        g2.setStroke(new BasicStroke(thickness));
+        g2.draw(new RoundRectangle2D.Double(x, y, width - 1, height - 1, radius, radius));
+        g2.dispose();
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(radius / 2, radius / 2, radius / 2, radius / 2);
+    }
+
+    @Override
+    public boolean isBorderOpaque() {
+        return false;
+    }
+}
