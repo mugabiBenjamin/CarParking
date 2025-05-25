bdfd4fd57e5423f1cc9365837528fe1f4d8dcc50
mugabiBenjamin <mugabiben6@gmail.com>
2025-05-25 20:38:06 +0300

    Enhance Car class by making fields final and adding parkedAt timestamp

--- src/model/Car.java
+++ src/model/Car.java
@@ -1,19 +1,24 @@
 package model;
 
+import util.Validator;
+
 public class Car {
-    // License plate of the car
-    private String plateNumber;
+    private final String plateNumber;
+    private final long parkedAt;
 
     public Car(String plateNumber) {
+        if (!Validator.isValidPlate(plateNumber)) {
+            throw new IllegalArgumentException("Invalid license plate format");
+        }
         this.plateNumber = plateNumber;
+        this.parkedAt = System.currentTimeMillis();
     }
 
     public String getPlateNumber() {
         return plateNumber;
     }
 
-    @Override
-    public String toString() {
-        return plateNumber;
+    public long getParkedAt() {
+        return parkedAt;
     }
 }
