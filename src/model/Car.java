bd4b13543788f7c4b64801cbba2795a644a30300
mugabiBenjamin <mugabiben6@gmail.com>
2026-07-26 16:51:40 +0300

    Refactor Car model improving license plate validation

--- src/model/Car.java
+++ src/model/Car.java
@@ -1,10 +1,20 @@
 package model;
 
-public class Car {
+import util.Validator;
+
+import java.util.Objects;
+
+public final class Car {
     private final String licensePlate;
 
     public Car(String licensePlate) {
-        this.licensePlate = licensePlate;
+        String normalizedPlate = Validator.normalizePlate(licensePlate);
+
+        if (!Validator.isValidPlate(normalizedPlate)) {
+            throw new IllegalArgumentException("Invalid license plate");
+        }
+
+        this.licensePlate = normalizedPlate;
     }
 
     public String getLicensePlate() {
@@ -13,5 +23,28 @@ public class Car {
 
     public String getPlateNumber() {
         return licensePlate;
+    }
+
+    @Override
+    public String toString() {
+        return licensePlate;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (!(object instanceof Car car)) {
+            return false;
+        }
+
+        return Objects.equals(licensePlate, car.licensePlate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(licensePlate);
     }
 }
